1ababc7d7a114f9e45ef48b336cefdec1b1d8163
Yoelvys <yoelvys.perez@dspot.com.pl>
2025-12-15 12:03:49 -0500

    refactor: restructure home and projects pages to utilize feature flags and remove deprecated components
    - Replace HomeView and ProjectsView with direct component imports in RootPage and AllProjectsPage
    - Implement feature flags to conditionally render sections in the home page and handle project visibility
    - Update navigation items to use absolute paths for improved routing
    - Refactor Navigator and SlidingMenu components to use Link for navigation instead of button clicks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import { metadata as seo } from '@/constants/metadata';
 import { Metadata } from 'next';
-import HomeView from '@/components/sections/home/HomeView';
+import { featureFlags } from '@/constants/featureFlags';
+
+import { Header } from '@/components/sections/home/Header';
+import { About } from '@/components/sections/home/About';
+import { Projects } from '@/components/sections/home/Projects';
+import { Experience } from '@/components/sections/home/Experience';
+import { Skills } from '@/components/sections/home/Skills';
+import { Contact } from '@/components/sections/home/Contact';
 
 export const metadata: Metadata = {
   ...seo,
@@ -12,5 +19,14 @@ export const metadata: Metadata = {
 };
 
 export default function RootPage() {
-  return <HomeView />;
+  return (
+    <main id="main">
+      {featureFlags.showHeader && <Header />}
+      {featureFlags.showAbout && <About />}
+      {featureFlags.showProjects && <Projects />}
+      {featureFlags.showExperience && <Experience />}
+      {featureFlags.showSkills && <Skills />}
+      {featureFlags.showContact && <Contact />}
+    </main>
+  );
 }
