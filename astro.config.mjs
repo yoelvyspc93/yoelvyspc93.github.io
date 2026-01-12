a9c0de07f605636c9d3210ec0aaec0d2b4744b36
Yoelvys <yoelvys.perez@dspot.com.pl>
2026-01-12 15:30:01 -0500

    Update Astro configuration and implement GitHub Actions for deployment
    - Updated astro.config.mjs to include site URL, base path, and trailing slash settings.
    - Added a new GitHub Actions workflow for deploying the site to GitHub Pages.
    - Removed the old static deployment workflow.
    - Adjusted image paths in components to reference the public directory for better asset management.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
-});
+	site: 'https://yoelvyspc93.github.io',
+	base: '/portfolio-astro',
+	trailingSlash: 'always',
+	vite: {
+		plugins: [tailwindcss()],
+	},
+})
