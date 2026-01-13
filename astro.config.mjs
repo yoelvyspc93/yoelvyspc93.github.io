5570927960597ef4d121c88d69f57dcaa4747f82
Yoelvys <yoelvys.perez@dspot.com.pl>
2026-01-13 15:56:46 -0500

    Refactor text styles across components for consistency
    - Updated text class names in various components to use a unified naming convention, replacing 'text-text-large' with 'text-large' and similar adjustments for other sizes.
    - Modified the Astro configuration to conditionally set site URL and base path based on the production environment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
 import tailwindcss from '@tailwindcss/vite'
+
+const isProduction = import.meta.env.PROD
+console.log('isProduction', isProduction)
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://yoelvyspc93.github.io',
-	base: '/portfolio-astro',
+	...(isProduction && {
+		site: 'https://yoelvyspc93.github.io',
+		base: '/portfolio-astro',
+	}),
 	trailingSlash: 'always',
 	vite: {
 		plugins: [tailwindcss()],
